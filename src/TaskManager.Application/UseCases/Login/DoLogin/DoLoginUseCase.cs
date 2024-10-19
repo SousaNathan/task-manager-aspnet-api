f505facc790a704b973b4be7aa62d48ae4242561
Nathan Sousa <nathan.wks@outlook.com>
2024-10-19 14:10:51 -0300

    add login use cases and dependencys

---
+++ src/TaskManager.Application/UseCases/Login/DoLogin/DoLoginUseCase.cs
@@ -0,0 +1,48 @@
+﻿using TaskManager.Communication.Requests;
+using TaskManager.Communication.Responses;
+using TaskManager.Domain.Repositories.User;
+using TaskManager.Domain.Security.Cryptography;
+using TaskManager.Domain.Security.Tokens;
+using TaskManager.Exception.ExceptionBase;
+
+namespace TaskManager.Application.UseCases.Login.DoLogin;
+
+public class DoLoginUseCase : IDoLoginUseCase
+{
+    private readonly IUserReadOnlyRepository userReadOnlyRepository;
+    private readonly IPasswordEncripter _passwordEncripter;
+    private readonly IAccessTokenGenerator _accessTokenGenerator;
+
+    public DoLoginUseCase(
+        IUserReadOnlyRepository readOnlyRepository,
+        IPasswordEncripter passwordEncripter,
+        IAccessTokenGenerator accessTokenGenerator)
+    {
+        _accessTokenGenerator = accessTokenGenerator;
+        userReadOnlyRepository = readOnlyRepository;
+        _passwordEncripter = passwordEncripter;
+    }
+
+    public async Task<ResponseRegisterUserJson> Execute(RequestLoginJson request)
+    {
+        var user = await userReadOnlyRepository.GetUserByEmail(request.Email);
+
+        if (user is null)
+        {
+            throw new InvalidLoginException();
+        }
+
+        var passwordMatch = _passwordEncripter.Verify(request.Password, user.Password);
+
+        if (!passwordMatch)
+        {
+            throw new InvalidLoginException();
+        }
+
+        return new ResponseRegisterUserJson
+        {
+            Name = user.Name,
+            Token = _accessTokenGenerator.Generate(user)
+        };
+    }
+}
---
+++ src/TaskManager.Application/UseCases/Login/DoLogin/IDoLoginUseCase.cs
@@ -0,0 +1,10 @@
+﻿using TaskManager.Communication.Requests;
+using TaskManager.Communication.Responses;
+
+namespace TaskManager.Application.UseCases.Login.DoLogin;
+
+public interface IDoLoginUseCase
+{
+    Task<ResponseRegisterUserJson> Execute(RequestLoginJson request);
+}
+
